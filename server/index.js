2ebf2865b5d729c190dda1aafe6b97dc4ec8db75
Preran <prean2003@gmail.com>
2025-07-02 02:50:50 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const server = require("http").createServer(app);
 
-server.listen(1337);
+// Serve static files from the React build (client/public)
+app.use(express.static(path.join(__dirname, '../client/public')));
 
-app.use(express.static(__dirname + "/.."));
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/../index.html");
+// Fallback route for client-side routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
 });
+
+server.listen(1337);
